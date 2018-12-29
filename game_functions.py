e18887a5d72c5443a2aec77cf34977e65ed8a752
Bryan Sarazin <bryan.sarazin@brightpeakfinancial.com>
2018-12-29 11:39:59 -0600

    refactor key events into modules

--- game_functions.py
+++ game_functions.py
@@ -10,18 +10,28 @@ def check_events(ship) -> None:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move ship to the right
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # Move ship to the left
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship) -> None:
+    """Respond to key presses"""
+    if event.key == pygame.K_RIGHT:
+        # Move ship to the right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # Move ship to the left
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship) -> None:
+    """Respond to key releases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship) -> None:
